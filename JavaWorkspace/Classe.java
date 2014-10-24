8982897b5c706991ef6d4c5d1d0be604adf6904a
Rafael Ciriaco <rafaelciriaco@gmail.com>
2014-10-23 22:26:43 -0200

    new class Classe.java exported

---
+++ JavaWorkspace/Classe.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class MeuPrograma {
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("OI");
+	}
+}
